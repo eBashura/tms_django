9afe39446da4447d43e0a2fbd581c260c0893956
Evgeny <evgenybashura@gmail.com>
2023-07-10 22:23:17 +0300

    some_changes

--- src/school/models.py
+++ src/school/models.py
@@ -12,6 +12,8 @@ class Student(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     group = models.ForeignKey(Group, null=True, blank=True, on_delete=models.SET_NULL, related_name='students')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.firstname} - {self.lastname}'
